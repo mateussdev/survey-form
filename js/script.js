1d83888c0db113d523ed662a8bed9bfe4743b8f7
mateussdev <mateusbs427@gmail.com>
2022-08-27 16:43:31 -0300

    :pencil: function study alert

---
+++ js/script.js
@@ -0,0 +1,3 @@
+function studyAlert() {
+  window.alert("Obrigado pela sua colaboração, mas esse formulário foi feito apenas com o intuito de praticar meus conhecimentos sobre formulários HTML. :)")
+}
